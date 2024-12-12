0d4bf53cdb7b54ec49a547e5e3a847c6c3c66440
AntonZaika <lycan44444@gmail.com>
2024-12-13 00:00:57 +0200

    feat(NEST): создание контроллеров и dto в Nest

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const BASE_PREFIX = 'api';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix(BASE_PREFIX);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
